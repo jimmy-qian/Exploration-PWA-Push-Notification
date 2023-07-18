980c36a55acbe80534cd7370f6ebc9ba0316294d
Jimmy Qian <jimmy.qian@labela.nl>
2023-07-18 09:35:51 +0200

    Initital commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import * as i from 'types';
+
+const Home: i.NextPageComponent = () => {
+  return (
+    <div>
+      <button>Request for permission</button>
+    </div>
+  );
+};
+
+export default Home;
